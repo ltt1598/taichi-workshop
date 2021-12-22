337a6809961ed67589adbc72d967d915ef7996e7
Tiantian Liu <ltt1598@gmail.com>
2021-12-22 17:20:47 +0800

    initial commit

---
+++ N-body.py
@@ -0,0 +1,74 @@
+import taichi as ti
+ti.init(ti.cpu)
+
+# gravitational constant 6.67408e-11, using 1 for simplicity
+G = 1
+PI = 3.141592653
+TAU = 2*PI
+
+N = 3000    # number of planets
+m = 1       # unit mass
+v0 = 100    # init vel
+
+dt = 1e-4   # time-step size
+
+# pos, vel and force of the particles
+# Nx2 vectors
+pos = ti.Vector.field(2, ti.f32, N)
+vel = ti.Vector.field(2, ti.f32, N)
+#TODO define the force for the particles.
+
+@ti.kernel
+def initialize():   # initialize the particles on a torus: r1=0.2, r2=0.4
+    center=ti.Vector([0.5, 0.5])
+    for i in range(N):
+        theta = ti.random() * TAU
+        r = ti.sqrt(ti.random()) * 0.2 + 0.2 
+        # LOOKAT: offset is initialized using a random radian theta
+        offset = r * ti.Vector([ti.cos(theta), ti.sin(theta)])
+        pos[i] = center+offset
+        # LOOKAT: velocity in intialized to the tangent direction
+        vel[i] = ti.Vector([-offset.y, offset.x]) * v0
+
+@ti.kernel          # compute the force for each particle
+def compute_force():
+    # clear force
+    for i in range(N):
+        ...
+        #TODO: set the force of particle i to zero
+
+    # compute gravitational force
+    for i in range(N):
+        ...
+        #TODO: compute the gravitational force for all particles
+
+@ti.kernel
+def update():     #symplectic euler integration
+    for i in range(N):     
+        ...
+        #TODO: update velocity using acceleration
+        #TODO: update position using velocity
+
+if __name__ == "__main__":
+
+    paused = False
+    initialize()
+
+    gui = ti.GUI('N-body problem', (512, 512))
+
+    while gui.running:
+        for e in gui.get_events(ti.GUI.PRESS):
+            if e.key == ti.GUI.ESCAPE:
+                exit()
+            elif e.key == ti.GUI.SPACE:
+                paused = not paused
+            elif e.key == 'r':
+                initialize()
+
+        if (not paused):
+            compute_force()
+            update()
+
+        gui.clear(0x112F41)
+        gui.circles(pos.to_numpy(), color=0xfeffd4, radius=2)
+        gui.show()
